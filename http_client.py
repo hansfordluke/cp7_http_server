ee4dc41fd91365c47e8b81eed936c78c7e957d2f
Luke Hansford <lukewhansford@gmail.com>
2022-07-23 11:10:38 -0400

    "Directory Splicing"

--- http_client.py
+++ http_client.py
@@ -1,16 +1,37 @@
 import os
-os.system("pip install requests")
-os.system("pip install curses")
+os.system("pip3 install requests")
 
 from email import header
+from urllib.parse import urlparse
 import http.client
 import sys
 import requests
 
 from pip import main
 
+#url = "http://mylonelyacejourney.ace"
+url = "http://localhost:9999"
+
+def url_splitter(url):
+    # Create a list of each bit between slashes
+    slashparts = url.split('/')
+    # Now join back the first three sections 'http:', '' and 'example.com'
+    basename = '/'.join(slashparts[:3]) + '/'
+    # All except the last one
+    dirname = '/'.join(slashparts[:-1]) + '/'
+    print('slashparts = %s' % slashparts)
+    print('basename = %s' % basename)
+    print('dirname = %s' % dirname)
+
 def url_transformation(char):
-    
+    if char.isnumeric():
+        url = url.capitalise()
+        print("int")
+    elif char.isalpha():
+        url_splitter(url)
+        print("alpha")
+    else:
+        print("spec")
 
 def msg_input():
     msg = input("Enter Message: ")
@@ -26,8 +47,8 @@ def msg_input():
     print(msg)
     print(binaryStr)
 
-def run_client(URL):
-    rqst = requests.get(URL)
+def run_client(url):
+    rqst = requests.get(url)
     # connection = http.client.HTTPConnection("localhost:9999")
     # connection.request("GET", "/")
     # response = rqst.getresponse()
@@ -38,5 +59,6 @@ def run_client(URL):
 
 if __name__ == "__main__":
     msg_input()
-    URL = "http://localhost:9999"
-    run_client(URL)
+    url = "http://localhost:9999"
+    url_splitter(url)
+    run_client(url)
