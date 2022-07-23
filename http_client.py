f6e595fc8649b6ae8ddc619da88c44b1fc5a81b0
Luke Hansford <lukewhansford@gmail.com>
2022-07-23 09:02:19 -0400

    "http_client"

--- http_client.py
+++ http_client.py
@@ -1,5 +1,42 @@
+import os
+os.system("pip install requests")
+os.system("pip install curses")
+
+from email import header
+import http.client
+import sys
 import requests
 
-payload = {"URL_addition": "Ouch"}
-r = requests.get("http://httpbin.org/get", params = payload)
-print(r.url)
+from pip import main
+
+def url_transformation(char):
+    
+
+def msg_input():
+    msg = input("Enter Message: ")
+    binaryStr = ' '.join(format(ord(item), 'b') for item in msg)
+    for char in msg:
+        if char.isnumeric():
+            print("int")
+        elif char.isalpha():
+            print("alpha")
+        else:
+            print("spec")
+    
+    print(msg)
+    print(binaryStr)
+
+def run_client(URL):
+    rqst = requests.get(URL)
+    # connection = http.client.HTTPConnection("localhost:9999")
+    # connection.request("GET", "/")
+    # response = rqst.getresponse()
+    # print("Status: {} and reason: {}".format(response.status, response.reason))
+    print(rqst)
+    # connection.close()
+    return rqst
+
+if __name__ == "__main__":
+    msg_input()
+    URL = "http://localhost:9999"
+    run_client(URL)
