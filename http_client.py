75261477339da37696d117519a3d03b51934fd2a
Luke Hansford <lukewhansford@gmail.com>
2022-07-23 16:35:13 -0400

    "zero error fixed"

--- http_client.py
+++ http_client.py
@@ -20,7 +20,7 @@ def randomised_word(msg_to_hex): #The function responsible for generating the wo
         for char in hex_values: #Split the string away from the 0x hex indicator
             print(f"Hex value {hex_values} split for hex_word_length: " + char)
             lowercase_letters = string.ascii_lowercase
-            hex_word_length = int(char, 16) #The length of the hex value as an int
+            hex_word_length = int(char, 16) + 1 #The length of the hex value as an int
             hex_word = ''
             while len(hex_word) != hex_word_length:
                 hex_word += random.choice(lowercase_letters) #Selects a random character on each iteration
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     hex_word, hex_wordlist = randomised_word(input) # words used to represent hex value length from 1-16
     payload = ''    
     for position, hex_word in enumerate(hex_wordlist): 
-        special_char = ["-", "_", "&", "%", "@", "#", "$", "!"] # special char used to separate hex words
+        special_char = ["-", "_", "&", "%", "@", "$", "!"] # special char used to separate hex words
         file_type = [".jpg", ".zip", ".txt", ".pdf", ".html"] # file type to close the url
         if position % 2 == 0: 
             payload += hex_word + f"{random.choice(special_char)}" # special_char separates the hex characters
