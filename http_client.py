3f20a02254ef783af2a6ed1d213b8b9d18222843
Luke Hansford <lukewhansford@gmail.com>
2022-07-23 01:06:04 -0400

    "Adding Functionality"

--- http_client.py
+++ http_client.py
@@ -1,8 +1,28 @@
+import os
+os.system("pip install requests")
+
+from email import header
 import http.client
+import sys
+import requests
 
-connection = http.client.HTTPConnection("localhost:9999")
-connection.request("GET", "/")
-response = connection.getresponse()
-print("Status: {} and reason: {}".format(response.status, response.reason))
+from pip import main
 
-connection.close()
+def msg_input():
+    msg = input("Enter Message: ")
+    print(msg)
+
+def run_client(URL):
+    rqst = requests.get(URL)
+    # connection = http.client.HTTPConnection("localhost:9999")
+    # connection.request("GET", "/")
+    # response = rqst.getresponse()
+    # print("Status: {} and reason: {}".format(response.status, response.reason))
+    print(rqst)
+    # connection.close()
+    return rqst
+
+if __name__ == "__main__":
+    msg_input()
+    URL = "http://localhost:9999"
+    run_client(URL)
--- http_server.py
+++ http_server.py
@@ -1,6 +1,13 @@
+import os
+os.system("pip install requests")
+os.system("pip install requests_toolbelt")
+
 from http import server
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from re import L
+import http.server
+from requests_toolbelt.utils import dump
+import requests
 
 HOST = "localhost"
 PORT = 9999
@@ -8,15 +15,20 @@ PORT = 9999
 class TestHTTP(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(404)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-
         self.wfile.write(bytes("<html><body><h1>HELLO WORLD!</h1></body></html>", "utf-8"))
 
-
-
+    
 server = HTTPServer((HOST, PORT), TestHTTP)
+# response = requests.Request('POST', "http://localhost:9999")
+
+resp = requests.get('http://localhost:9999')
+data = dump.dump_all(resp)
+print(data.decode('utf-8'))
+
 print("Server running...")
+# print(response.url)
 server.serve_forever()
 server.server_close()
